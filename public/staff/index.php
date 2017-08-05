9c40c04ae20676fe6685ae680711fc2164e567d3
da5atar <mass.sow@gmail.com>
2017-08-05 22:43:52 +0000

    Create and style the first page

--- public/staff/index.php
+++ public/staff/index.php
@@ -2,9 +2,32 @@
 
 <html lang="en">
   <head>
-    <title></title>
+    <title>Globe Bank International</title>
     <meta charset="utf-8">
+    <!-- Styles -->
+    <link rel="stylesheet" media="all" href="../stylesheets/staff.css">
   </head>
+    <header>
+      <h1>
+        GBI Staff Area
+      </h1>
+
+    </header>
+
+    <navigation>
+      <ul>
+        <li>
+          <a href="index.php">Menu</a>
+        </li>
+      </ul>
+    </navigation>
+    <div id="content">
+
+    </div>
+
+    <footer>
+      &copy; <?php echo date('Y'); ?> Globe Bank
+    </footer>
 
   <body>
 
